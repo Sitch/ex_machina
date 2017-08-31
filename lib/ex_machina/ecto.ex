64305098aa111281f1919b81486396353ec36c3e
Sitch <Sitch@users.noreply.github.com>
2017-08-31 17:12:40 -0400

    Check for Enumerable implementation

--- lib/ex_machina/ecto.ex
+++ lib/ex_machina/ecto.ex
@@ -317,8 +317,12 @@ defmodule ExMachina.Ecto do
     Enum.map(values, &convert_atom_keys_to_strings/1)
   end
   defp convert_atom_keys_to_strings(record) when is_map(record) do
-    Enum.reduce record, Map.new, fn({key, value}, acc) ->
-      Map.put(acc, to_string(key), convert_atom_keys_to_strings(value))
+    if Enumerable.impl_for(record) do
+      Enum.reduce record, Map.new, fn({key, value}, acc) ->
+        Map.put(acc, to_string(key), convert_atom_keys_to_strings(value))
+      end
+    else
+      record
     end
   end
   defp convert_atom_keys_to_strings(value), do: value
